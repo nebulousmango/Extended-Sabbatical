42d1b75b4df66c20f08d51eb5c0344ddc3b9776b
nebulousmango <sitaradaniel@gmail.com>
2024-05-13 23:55:09 +0530

    Cursor update

---
+++ Extended Sabbatical/Assets/Scripts/CursorManager.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+    public Texture2D cursorTex;    
+    void Awake() { Cursor.SetCursor(cursorTex, Vector2.zero, CursorMode.ForceSoftware);  }
+}
